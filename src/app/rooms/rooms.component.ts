3f12f52377a300e798c287ae00a62e7cab9f01d8
Krzysztof Sobiecki <ksobiecki11@gmail.com>
2020-11-28 01:04:03 +0100

    Destroy programme

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -2,16 +2,7 @@ import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Room } from './room/room.model';
 
-@Component({
-  selector: 'app-rooms',
-  templateUrl: './rooms.template.html',
-  styleUrls: ['./rooms.less'],
-})
-export class RoomsComponent {
-  rooms: Room[] = [
-    { id: 1111, name: 'Room1', author: 'Krzyś' },
-    { id: 2222, name: 'Room2', author: 'Piter' },
-    { id: 3333, name: 'Room3', author: 'Pyć' },
+
   ];
 
   onSubmit(form: NgForm) {
