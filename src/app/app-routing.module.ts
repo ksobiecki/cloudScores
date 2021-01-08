421d83e4619464d4ad942b82633c5e3cf0ae1300
RudeDuck <pikter1232@gmail.com>
2021-01-08 20:46:05 +0100

    Add basic login screen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { GamesComponent } from './games/games.component';
 import { GameComponent } from './games/game/game.component';
 import { RoomsComponent } from './rooms/rooms.component';
+import {LoginComponent} from './auth/login/login.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/rooms', pathMatch: 'full' },
   { path: 'rooms', component: RoomsComponent },
   { path: 'room/:name/games', component: GamesComponent },
   { path: 'room/:name/games/game:id', component: GameComponent },
+  { path: 'login', component: LoginComponent },
 ];
 
 @NgModule({
---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+import {NgForm} from '@angular/forms';
+
+@Component({
+  templateUrl: 'login.component.html',
+  styleUrls: ['login.component.less']
+})
+export class LoginComponent {
+  isLoading = false;
+
+  onLogin(form: NgForm): void {
+    console.log(form.value);
+  }
+}
