aef828b84de3fe0a0c0ab9f99336f96994df757f
Krzysztof Sobiecki <ksobiecki11@gmail.com>
2021-01-21 18:40:54 +0100

    Add dashboard aside structure

--- src/app/menu/menu-dashboard/menu-dashboard.component.ts
+++ src/app/menu/menu-dashboard/menu-dashboard.component.ts
@@ -1,10 +1,10 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-menu-dashboard',
-    templateUrl: './menu-dashboard.template.html',
-    styleUrls: ['./menu-dashboard.less']
+  selector: 'app-menu-dashboard',
+  templateUrl: './menu-dashboard.template.html',
+  styleUrls: ['./menu-dashboard.less'],
 })
 export class MenuDashboardComponent {
-
+  chosenGame: string = 'f';
 }
