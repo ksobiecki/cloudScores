aeb6b7ecfba10ded3d089e2203a9594cad38d4b2
RudeDuck <pikter1232@gmail.com>
2021-01-23 19:23:53 +0100

    change user model, fix bugs

--- src/shared/services/login.service.ts
+++ src/shared/services/login.service.ts
@@ -6,10 +6,11 @@ import {Router} from '@angular/router';
 @Injectable({providedIn: 'root'})
 export class LoginService {
   users: User[] = [{
-    firstName: 'Jan',
-    lastName: 'Kowalski',
+    _id: '123',
+    username: 'jankowalski',
     email: '123@123.com',
-    password: '123'
+    password: '123',
+    rooms_id: []
   }];
   currentUser = null;
   isUserLoggedIn = false;
@@ -21,10 +22,10 @@ export class LoginService {
     this.users.push(user);
     return 0;
   }
-  public login(email: string, password: string): number {
-    for(const user of this.users) {
-      if(user.email === email && user.password === password) {
-        this.currentUser = user;
+  public login(user: User): number {
+    for(const existingUser of this.users) {
+      if(user.email === existingUser.email && user.password === existingUser.password) {
+        this.currentUser = existingUser;
         this.isUserLoggedIn = true;
         return 0;
       }
@@ -33,6 +34,9 @@ export class LoginService {
   }
   public getIsUserLoggedIn(): boolean {
     return this.isUserLoggedIn;
+  }
+  public getUsername(): string {
+    return this.currentUser.username;
   }
   public logOut(): void {
     this.isUserLoggedIn = false;
