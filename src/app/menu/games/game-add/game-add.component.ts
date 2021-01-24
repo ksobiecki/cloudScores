4442d96fb6fbf9b79e78d1fe9efe25fdc6dfe07a
RudeDuck <pikter1232@gmail.com>
2021-01-24 16:24:55 +0100

    Add game modal almost finished

--- src/app/menu/games/game-add/game-add.component.ts
+++ src/app/menu/games/game-add/game-add.component.ts
@@ -1,46 +1,110 @@
-import {Component, Inject, OnInit} from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
-import { Room } from 'src/shared/models/room.model';
-import { RoomsService } from 'src/shared/services/rooms.service';
-import { Game } from '../../../../shared/models/game.model';
-import { faTimes } from '@fortawesome/free-solid-svg-icons';
-import { MatDialog } from '@angular/material/dialog';
+import {AfterViewInit, Component, Inject, OnInit, ViewChild} from '@angular/core';
+import {NgForm} from '@angular/forms';
+import {ActivatedRoute, Router} from '@angular/router';
+import {Room} from 'src/shared/models/room.model';
+import {RoomsService} from 'src/shared/services/rooms.service';
+import {Game} from '../../../../shared/models/game.model';
+import {faTimes} from '@fortawesome/free-solid-svg-icons';
+import {MatDialog} from '@angular/material/dialog';
 import {MAT_DIALOG_DATA} from '@angular/material/dialog';
 import {Subscription} from 'rxjs';
+import {MatTableDataSource} from '@angular/material/table';
+import {MatPaginator} from '@angular/material/paginator';
+import {SelectionModel} from '@angular/cdk/collections';
+import {MatCheckbox} from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-game-add',
   templateUrl: './game-add.template.html',
   styleUrls: ['./game-add.less'],
 })
-export class GameAddComponent implements OnInit {
+export class GameAddComponent implements OnInit, AfterViewInit {
   faClose = faTimes;
+  allGames: Game[] = [];
   games: Game[] = [];
   gameSubscription: Subscription;
+  dataSource = new MatTableDataSource<Game>(this.games);
+  displayedColumns = ['select', 'name', 'image'];
+  selection = new SelectionModel<Game>(false, []);
+  isAnythingSelected = false;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
   constructor(
     private roomsService: RoomsService,
     private route: ActivatedRoute,
     private dialog: MatDialog,
     @Inject(MAT_DIALOG_DATA) public data: any
-  ) {}
+  ) {
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+  }
 
   ngOnInit(): void {
     this.roomsService.getAllGames();
     this.gameSubscription = this.roomsService
       .getAllGamesUpdateListener()
-      .subscribe((games: Game[]) => (this.games = games));
+      .subscribe((games: Game[]) => {
+        this.allGames = games;
+        this.updateGames();
+      });
     const allGames: Game[] = this.roomsService.allGames;
-    console.log(allGames);
+    this.dataSource.paginator = this.paginator;
   }
 
-  onSubmit(form: NgForm) {
+  onSubmit(form: NgForm): void {
     const currentRoom: Room = this.data.currentRoom;
     console.log(currentRoom);
+    const chosenGame = this.selection.selected[0];
+    chosenGame.imgUrl = chosenGame.imgUrl.substring(3);
+    console.log(chosenGame);
+    this.roomsService.addGame(currentRoom, chosenGame);
     // this.roomsService.addGame(currentRoom, game);
   }
 
   closeModal(): void {
     const thisDialogRef = this.dialog.closeAll();
+  }
+
+  /** Whether the number of selected elements matches the total number of rows. */
+  isAllSelected(): boolean {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
+  }
+
+  /** Selects all rows if they are not all selected; otherwise clear selection. */
+  masterToggle(): void {
+    this.isAllSelected() ?
+      this.selection.clear() :
+      this.dataSource.data.forEach(row => this.selection.select(row));
+  }
+
+  /** The label for the checkbox on the passed row */
+  checkboxLabel(row?: Game): string {
+    if (this.selection.selected.length > 0) {
+      this.isAnythingSelected = true;
+    } else {
+      this.isAnythingSelected = false;
+    }
+    if (!row) {
+      return `${this.isAllSelected() ? 'select' : 'deselect'} all`;
+    }
+    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.name}`;
+  }
+
+  updateGames(): void {
+    const input = (document.getElementById('name') as HTMLInputElement).value.toLowerCase();
+    this.games = [];
+    const allGamesCopy = this.allGames.map(obj => ({...obj}));
+    for (const game of allGamesCopy) {
+      game.imgUrl = '../' + game.imgUrl;
+      if (input === '' || game.name.toLowerCase().indexOf(input) === 0) {
+        this.games.push(game);
+      }
+    }
+    console.log(this.games);
+    this.dataSource = new MatTableDataSource<Game>(this.games);
   }
 }
