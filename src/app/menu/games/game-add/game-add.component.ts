ceba4710cdd7332130df2506f6abaf459317b3ac
RudeDuck <pikter1232@gmail.com>
2021-01-24 13:48:19 +0100

    Get all games into table inside component

--- src/app/menu/games/game-add/game-add.component.ts
+++ src/app/menu/games/game-add/game-add.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Room } from 'src/shared/models/room.model';
@@ -7,14 +7,17 @@ import { Game } from '../../../../shared/models/game.model';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import { MatDialog } from '@angular/material/dialog';
 import {MAT_DIALOG_DATA} from '@angular/material/dialog';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-game-add',
   templateUrl: './game-add.template.html',
   styleUrls: ['./game-add.less'],
 })
-export class GameAddComponent {
+export class GameAddComponent implements OnInit {
   faClose = faTimes;
+  games: Game[] = [];
+  gameSubscription: Subscription;
   constructor(
     private roomsService: RoomsService,
     private route: ActivatedRoute,
@@ -24,6 +27,9 @@ export class GameAddComponent {
 
   ngOnInit(): void {
     this.roomsService.getAllGames();
+    this.gameSubscription = this.roomsService
+      .getAllGamesUpdateListener()
+      .subscribe((games: Game[]) => (this.games = games));
     const allGames: Game[] = this.roomsService.allGames;
     console.log(allGames);
   }
