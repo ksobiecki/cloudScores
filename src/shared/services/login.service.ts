e60897b007473540cbc4df42f74cdc84119ce44f
Krzysztof Sobiecki <ksobiecki11@gmail.com>
2021-01-24 11:04:41 +0100

    Refactor rooms.serviuce

--- src/shared/services/login.service.ts
+++ src/shared/services/login.service.ts
@@ -1,48 +1,47 @@
-import {Injectable, ɵɵresolveBody} from '@angular/core';
-import {User} from '../models/user.model';
-//import {Observable} from 'rxjs';
-import {Router} from '@angular/router';
+import { Injectable, ɵɵresolveBody } from '@angular/core';
+import { User } from '../models/user.model';
+import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
-
 
 @Injectable({ providedIn: 'root' })
 export class LoginService {
-
   currentUser = null;
   isUserLoggedIn = false;
 
-  constructor(public router: Router, private http: HttpClient) {
-  }
+  constructor(public router: Router, private http: HttpClient) {}
 
   public createUser(user: User): number {
-        this.http
-        .post<{ message: string }>(
-          'http://localhost:3000/api/users', user
-        )
-        .subscribe((responseData) => {
-          console.log(responseData.message);
-        });
-        return 0; // user created
+    this.http
+      .post<{ message: string }>('http://localhost:3000/api/users', user)
+      .subscribe((responseData) => {
+        console.log(responseData.message);
+      });
+    return 0; // user created
   }
+
   public login(user: User): Promise<number> {
     return new Promise((resolve, reject) => {
-    this.http
-    .post<{ message: string }>(
-      'http://localhost:3000/api/users/login', user
-      , {
-        observe: 'body',
-        responseType: 'json'
-      }
-    )
-    .subscribe((postData: any) => {
-      if(user.email === postData.user.email && user.password === postData.user.password) {
-        this.currentUser = postData.user;
-        this.isUserLoggedIn = true;
-        resolve(0);
-      }
+      this.http
+        .post<{ message: string }>(
+          'http://localhost:3000/api/users/login',
+          user,
+          {
+            observe: 'body',
+            responseType: 'json',
+          }
+        )
+        .subscribe((postData: any) => {
+          if (
+            user.email === postData.user.email &&
+            user.password === postData.user.password
+          ) {
+            this.currentUser = postData.user;
+            this.isUserLoggedIn = true;
+            resolve(0);
+          }
+        });
     });
-  });
-}
+  }
 
   public getIsUserLoggedIn(): boolean {
     return this.isUserLoggedIn;
